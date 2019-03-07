4eaad29eb9310973dc4dbd0899cf75dea86ed563
adico1 <adico1@gmail.com>
2019-03-07 14:17:02 +0200

    add a-z.js file with spec as comment

---
+++ a-z.js
@@ -0,0 +1,7 @@
+/**
+ * the project will end with logging the letters a-z to the console.
+ * each letter will be outputted in a diffent line and console.log command.
+ * There are 4 people working on the project.
+ * Each branch will add no more the 2 non-consequtive letters 
+ * and will remove them from the a-z one liner
+ */
