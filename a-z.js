6f2561e9cd8abe66c4dd21abde6234f083232751
adico1 <adico1@gmail.com>
2019-03-07 15:11:33 +0200

    add log a-z in one liner

--- a-z.js
+++ a-z.js
@@ -1,7 +1,8 @@
 /**
  * the project will end with logging the letters a-z to the console.
  * each letter will be outputted in a diffent line and console.log command.
- * There are 4 people working on the project.
+ * There are 4 people working on the project: 1) af, 2) gl, 3) mr & 4) sz
  * Each branch will add no more the 2 non-consequtive letters 
  * and will remove them from the a-z one liner
  */
+console.log('abcdefghijklmnopqrstuvwxyz');
